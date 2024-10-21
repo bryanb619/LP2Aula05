cc8d107b6d542cbaf6a70735af4ff1ec0b7fecf3
Steven Hall <stevenbryanb.work@gmail.com>
2024-10-21 17:34:14 +0100

    Add TryCtach project

---
+++ TryCatch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            Run();
+        }
+
+
+        private static void Run()
+        {
+            Console.Write("Insira um número");
+
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Numero inserido: {i}");
+        }
+    }
+}
